1336ff81d8fb6ad182d808ab68785b48d621f5ea
Omar Gutierrez Sanchez <117556558+spirosis@users.noreply.github.com>
2023-11-16 01:14:08 +0000

    fetching todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
+	const [todo, setTodo] = useState('');
+	const [todos, setTodos] = useState([]);
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			
+			<input onChange={(evt) => setTodo(evt.target.value)} value={todo}/>
+			<button onCLick={() => setTodos(prev =>[...prev, todo])}>Send</button>
+			<ul>
+				{todos.map((data, idx) => <li key={`${data}-${idx}`}>{data}</li>)}
+			</ul>
 		</div>
 	);
 };
