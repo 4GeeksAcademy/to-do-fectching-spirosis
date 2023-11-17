7ab8b31065eed24d435283a9f75bd6ccad6ab08e
Omar Gutierrez Sanchez <117556558+spirosis@users.noreply.github.com>
2023-11-17 01:17:19 +0000

    fetching update but not perfect yet

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -15,14 +15,54 @@ const Home = () => {
 		setTodos(todos.filter((_, todoIdx)=> 
 		todoIdx !== idx))
 	}
+
+
+	useEffect(()=>{
+		const request = async () =>{
+			const res = await fetch("https://playground.4geeks.com/apis/fake/todos/user/spirosis",{
+				method: "POST",
+				body: JSON.stringify([]),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+			const data = await res.json()
+			console.log(data)
+		}
+		request()
+	},[])
 	
+
+	useEffect(()=>{
+		const request = async () =>{
+			const res = await fetch("https://playground.4geeks.com/apis/fake/todos/user/spirosis")
+			const data = await res.json()
+			setTodos(data)
+			console.log(data)
+		}
+		request()
+	},[])
+	// useEffect(() => {
+	// 	const aPromise = new Promise((resolve, reject) => {
+	// 		setTimeout(() => {
+	// 			reject(true)
+	// 		}, 1000)
+	// 		console.log("afdterimte")
+	// 	})
+	// 	console.log(aPromise)
+	// 	aPromise.then((data) => console.log(data, aPromise)).catch((err)=> console.log(err, aPromise))
+	// }, [])
+
 	return (
 		<div className="text-center">
 			
 			<input onChange={handleOnChange} value={todo} />
 			<button onClick={handleClick}>Send</button>
 			<ul>
-				{todos.map((data, idx) => <li key={`${data} - ${idx}`}>{data}
+				{todos.map(({done, id, label}, idx) => 
+				<li key={`${id}`}>
+					{done && ["done"]}
+					{label}
 				<button onClick={handleFilter(idx)}>X</button>
 				</li>)}
 			</ul>
